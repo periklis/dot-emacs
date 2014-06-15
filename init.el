e4852dd39c420d0cf1d432aa80dd7964daa3a476
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2014-06-15 18:47:48 +0200

    Adding emacs 23 bc for user-emacs-directory var

--- init.el
+++ init.el
@@ -3,6 +3,14 @@
 ;;
 (setq inhibit-startup-message t)
 
+;; Adding backported emacs user dir var for emacs versions < 23.x
+(unless (boundp 'user-emacs-directory)
+  (defvar user-emacs-directory "~/.emacs.d/"
+    "Directory beneath which additional per-user Emacs-specific files are placed.
+  Various programs in Emacs store information in this directory.
+  Note that this should end with a directory separator.
+  See also `locate-user-emacs-file'."))
+
 ;; Set path to dependencies
 (setq site-lisp-dir (expand-file-name "site-lisp" user-emacs-directory))
 
