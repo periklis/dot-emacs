5a1757ef21b717500279f8ded90a42f8cbedd56f
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2014-06-27 23:08:35 +0200

    Finalizing projectile key bindings

--- setup-projectile.el
+++ setup-projectile.el
@@ -12,6 +12,8 @@
 
 ;; Key binding for helm projectile search
 (global-set-key (kbd "C-c h") 'helm-projectile)
-(global-set-key (kbd "C-c m") 'projectile-find-tag)
+(global-set-key (kbd "C-c m") 'imenu-anywhere)
+(global-set-key (kbd "C-c s") 'projectile-switch-project)
+(global-set-key (kbd "C-c b") 'projectile-switch-to-buffer)
 
 (provide 'setup-projectile)
