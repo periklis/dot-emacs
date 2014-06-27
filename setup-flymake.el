7c5f9ef90c8091f8fc1b35c46b01522637b73353
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2014-06-27 17:14:32 +0200

    Fixing phpcs output

--- setup-flymake.el
+++ setup-flymake.el
@@ -1,12 +1,12 @@
 ;;
 ;; Flymake configuration
 ;;
-(require 'flymake)
-(require 'flymake-cursor)
 
+(setq flymake-log-file-name (concat temporary-file-directory "/flymake.log"))
+(setq flymake-log-level -1)
 (setq flymake-cursor-auto-enable t)
 (setq flymake-no-changes-timeout 5)
-(setq flymake-max-parallel-syntax-checks 8)
+(setq flymake-max-parallel-syntax-checks 12)
 (setq flymake-run-in-place nil)
 (setq flymake-number-of-errors-to-display nil)
 
