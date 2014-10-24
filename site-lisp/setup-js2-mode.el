b14133ce3b02960fbe557b725364630f6c12cb69
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2014-10-24 16:40:45 +0200

    Fix auto-complete on js2-mode

--- site-lisp/setup-js2-mode.el
+++ site-lisp/setup-js2-mode.el
@@ -11,6 +11,7 @@
 (add-hook 'js2-mode-hook '(lambda () (subword-mode 1)))
 (add-hook 'js2-mode-hook '(lambda () (linum-mode 1)))
 (add-hook 'js2-mode-hook '(lambda () (setq truncate-lines 0)))
+(add-hook 'js2-mode-hook 'ac-js2-mode)
 
 (provide 'setup-js2-mode)
 ;;; setup-js2-mode.el ends here
