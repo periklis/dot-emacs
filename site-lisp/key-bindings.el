32b481bf1eff5d6748a3813225d9c65106852378
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2014-10-29 08:53:29 +0100

    Improve mode specific keys to use C-c prefix convention

--- site-lisp/key-bindings.el
+++ site-lisp/key-bindings.el
@@ -20,29 +20,28 @@
 (global-set-key (kbd "RET") 'newline-and-indent)
 
 ;; duplicate line command
-(global-set-key "\C-c\C-d" "\C-a\C- \C-n\M-w\C-y") 
+(global-set-key "\C-c\C-d" "\C-a\C- \C-n\M-w\C-y")
 
 ;; expand region
 (global-set-key (kbd "C-=") 'er/expand-region)
-
 ;; helm
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-m") 'helm-M-x)
-(global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-c h o") 'helm-occur)
-(global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
+(global-set-key (kbd "C-c h SPC") 'helm-all-mark-rings)
 (global-set-key (kbd "C-c h g") 'helm-google-suggest)
 (global-set-key (kbd "C-c h b") 'helm-descbinds)
-(global-set-key (kbd "C-h a") 'helm-ack)
-(global-set-key (kbd "C-h f") 'helm-apropos)
-(global-set-key (kbd "C-h r") 'helm-info-emacs)
-(global-set-key (kbd "C-h C-l") 'helm-locate-library)
+(global-set-key (kbd "C-c h a") 'helm-ack)
+(global-set-key (kbd "C-c h f") 'helm-apropos)
+(global-set-key (kbd "C-c h d") 'helm-info-emacs)
+(global-set-key (kbd "C-c h l") 'helm-locate-library)
+(global-set-key (kbd "C-c h i") 'helm-semantic-or-imenu)
 
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
@@ -57,15 +56,14 @@
 (global-set-key (kbd "C-;") 'ac-complete-with-helm)
 
 ;; helm-projectile and projectile
-(global-set-key (kbd "C-c h") 'helm-projectile)
-(global-set-key (kbd "C-c m") 'imenu-anywhere)
-(global-set-key (kbd "C-c s") 'projectile-switch-project)
-(global-set-key (kbd "C-c b") 'projectile-switch-to-buffer)
-(global-set-key (kbd "C-c e") 'helm-flycheck)
+(global-set-key (kbd "C-c h p") 'helm-projectile)
+(global-set-key (kbd "C-c p s") 'projectile-switch-project)
+(global-set-key (kbd "C-c p b") 'projectile-switch-to-buffer)
+(global-set-key (kbd "C-c f e") 'helm-flycheck)
 (define-key projectile-mode-map (kbd "s-s") 'projectile-persp-switch-project)
 
 ;; magit
-(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-c m g") 'magit-status)
 
 (provide 'key-bindings)
 ;;; key-bindings.el ends here
