ab72bbfd08158b576d25b1ff82b4c533fa0ff0fe
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2015-12-24 18:03:27 +0100

    Add elfeed to org integration

--- init.el
+++ init.el
@@ -227,7 +227,36 @@
   (setq-default elfeed-search-filter "@1-week-ago +unread ")
   (add-hook 'elfeed-new-entry-hook
             (elfeed-make-tagger :before "1 weeks ago"
-                                :remove 'unread)))
+                                :remove 'unread))
+
+  (defun elfeed-link-title (entry)
+    "Copy the entry title and URL as org link to the clipboard."
+    (interactive)
+    (let* ((link (elfeed-entry-link entry))
+           (title (elfeed-entry-title entry))
+           (titlelink (concat "[[" link "][" title "]]")))
+      (when titlelink
+        (kill-new titlelink)
+        (x-set-selection 'PRIMARY titlelink)
+        (message "Yanked: %s" titlelink))))
+
+  (defun elfeed-search-link-title ()
+    "Copy the current entry title and URL as org link to the clipboard."
+    (interactive)
+    (let ((entries (elfeed-search-selected)))
+      (cl-loop for entry in entries
+               when (elfeed-entry-link entry)
+               do (elfeed-link-title entry))))
+
+  (defun elfeed-show-link-title ()
+    "Copy the current entry title and URL as org link to the clipboard."
+    (interactive)
+    (elfeed-link-title elfeed-show-entry))
+
+  (bind-keys :map elfeed-show-mode-map
+             ("l" . elfeed-show-link-title))
+  (bind-keys :map elfeed-search-mode-map
+             ("l" . elfeed-search-link-title)))
 
 (use-package emacs-eclim
   :ensure t
