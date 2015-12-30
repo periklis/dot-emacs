cb259146fc569e5de2ecbccb623ee95ee5d78daa
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2015-12-30 15:19:02 +0100

    Add lazy loading org-mode

--- init.el
+++ init.el
@@ -775,13 +775,12 @@
 
 (use-package org
   :ensure t
-  :commands org-mode
+  :mode ("\\.org\\'" . org-mode)
   :init
   (use-package orgit :ensure t :commands projectile-vc)
   (use-package org-projectile
     :ensure t
-    :bind (("C-c n p" . org-projectile:project-todo-completing-read)
-           ("C-c c" . org-capture))
+    :bind ("C-c n p" . org-projectile:project-todo-completing-read)
     :config
     (setq org-agenda-files (append org-agenda-files (org-projectile:todo-files)))
     (add-to-list
