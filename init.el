32e1ac7d57b1bd99571cca850413c010de5a5012
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2016-11-12 18:55:33 +0100

    Add missing quote

--- init.el
+++ init.el
@@ -693,16 +693,16 @@
   :config
   (custom-set-variables
    '(hippie-expand-try-functions-list
-     (try-expand-dabbrev
-      try-expand-dabbrev-all-buffers
-      try-expand-dabbrev-from-kill
-      try-complete-file-name-partially
-      try-complete-file-name
-      try-expand-all-abbrevs
-      try-expand-list
-      try-expand-line
-      try-complete-lisp-symbol-partially
-      try-complete-lisp-symbol))))
+     '(try-expand-dabbrev
+       try-expand-dabbrev-all-buffers
+       try-expand-dabbrev-from-kill
+       try-complete-file-name-partially
+       try-complete-file-name
+       try-expand-all-abbrevs
+       try-expand-list
+       try-expand-line
+       try-complete-lisp-symbol-partially
+       try-complete-lisp-symbol))))
 
 (use-package geben
   :ensure t
