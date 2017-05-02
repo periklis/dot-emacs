f16f7cf801c903a0595f60b6d5141ac008e93eb9
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2017-05-02 16:00:30 +0200

    Setup tide in web-mode for tsx files

--- init.el
+++ init.el
@@ -1328,7 +1328,12 @@
               (eldoc-mode +1)))
   (add-hook 'typescript-mode-hook #'flyspell-prog-mode)
 
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (tide-setup))))
+  )
 
 (use-package tramp
   :demand t
