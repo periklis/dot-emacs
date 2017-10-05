4d288cbe6eac9cc6666450c15b3519c5e9e8ff44
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2017-10-05 15:13:31 +0200

    Disable ensime aggressive highlighting for implicits

--- init.el
+++ init.el
@@ -419,7 +419,24 @@
   (use-package scala-mode :ensure t)
   (custom-set-variables
    '(ensime-startup-notification nil)
-   '(ensime-startup-snapshot-notification nil)))
+   '(ensime-startup-snapshot-notification nil)
+   '(ensime-startup-snapshot-notification-3)
+   '(ensime-overlays-use-font-lock t)
+   '(ensime-sem-high-faces
+     '((var . scala-font-lock:var-face)
+       (val . (:inherit font-lock-constant-face :slant italic))
+       (varField . scala-font-lock:var-face)
+       (valField . (:inherit font-lock-constant-face :slant italic))
+       (functionCall . font-lock-function-name-face)
+       (operator . font-lock-keyword-face)
+       (param . (:slant italic))
+       (class . font-lock-type-face)
+       (trait .  (:inherit font-lock-type-face :slant italic))
+       (object . font-lock-constant-face)
+       (package . font-lock-preprocessor-face)
+       (implicitConversion . nil)
+       (implicitParams . nil)
+       (deprecated . (:strike-through "dark gray"))))))
 
 (use-package eshell
   :config
